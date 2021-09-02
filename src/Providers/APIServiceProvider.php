0fa460f9de2ffb1bd9760689664538f18c0280bf
Thomas Alrek <thomas@alrek.no>
2021-09-02 16:30:44 +0200

    Add ability to resolve multiple connections

--- src/Providers/APIServiceProvider.php
+++ src/Providers/APIServiceProvider.php
@@ -4,42 +4,30 @@ namespace Netflex\API\Providers;
 
 use Netflex\API\Client;
 use Netflex\API\Contracts\APIClient;
-use Netflex\API\Exceptions\MissingCredentialsException;
+use Netflex\API\APIClientResolver;
+use Netflex\API\Contracts\ClientResolver;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class APIServiceProvider extends ServiceProvider
 {
   public function register()
   {
+    $this->app->bind(ClientResolver::class, APIClientResolver::class);
+
     $this->app->alias('api.client', Client::class);
     $this->app->alias('api.client', APIClient::class);
 
     $this->app->singleton('api.client', function () {
-      $publicKey = $this->app['config']['api.publicKey'] ?? null;
-      $privateKey = $this->app['config']['api.privateKey'] ?? null;
-      $baseUri = $this->app['config']['api.baseUri'] ?? null;
-
-      if ($publicKey && $privateKey) {
-        $options = [
-          'base_uri' => $baseUri,
-          'auth' => [
-            $publicKey,
-            $privateKey,
-          ],
-        ];
-
-        return new Client($options);
-      }
-
-      throw new MissingCredentialsException();
+      return Client::connection('default');
     });
   }
 
   public function boot()
   {
     $this->publishes([
-      __DIR__ . '/../config/api.php' => config_path('api.php')
+      __DIR__ . '/../config/api.php' => App::configPath('api.php')
     ], 'config');
   }
 }
