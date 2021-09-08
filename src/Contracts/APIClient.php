f3ed29a72c4aebf8675723c5a0e306b7573e43ee
Thomas Alrek <thomas@alrek.no>
2021-09-08 10:16:15 +0200

    Refactor to use netflex/http internally

--- src/Contracts/APIClient.php
+++ src/Contracts/APIClient.php
@@ -2,38 +2,9 @@
 
 namespace Netflex\API\Contracts;
 
-interface APIClient
+use Netflex\Http\Contracts\HttpClient;
+
+interface APIClient extends HttpClient
 {
-  /**
-   * @param string $url
-   * @param boolean $assoc = false
-   * @return mixed
-   * @throws Exception
-   */
-  public function get($url, $assoc = false);
-
-  /**
-   * @param string $url
-   * @param array $payload = []
-   * @param boolean $assoc = false
-   * @return mixed
-   * @throws Exception
-   */
-  public function put($url, $payload = [], $assoc = false);
-
-  /**
-   * @param string $url
-   * @param array $payload = []
-   * @param boolean $assoc = false
-   * @return mixed
-   * @throws Exception
-   */
-  public function post($url, $payload = [], $assoc = false);
-
-  /**
-   * @param string $url
-   * @return mixed
-   * @throws Exception
-   */
-  public function delete($url, $assoc = false);
+  //
 }
