de95e4f22ae2b89524ae550d2c883be463cb2fd6
Thomas Alrek <thomas@alrek.no>
2021-09-28 20:14:44 +0200

    Add withCredentials method

--- src/Facades/API.php
+++ src/Facades/API.php
@@ -7,6 +7,7 @@ use Netflex\Http\Facades\Http as Facade;
 /**
  * @method static mixed setCredentials(array $options)
  * @method static \Netflex\API\Contracts\APIClient connection(string $connection)
+ * @method static \Netflex\API\Contracts\APIClient withCredentials(array $credentials)
  *
  * @see \Netflex\API\Client
  */
